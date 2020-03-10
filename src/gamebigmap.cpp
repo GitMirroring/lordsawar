e2cc7a1cd9fa27cd29d6cae6dfd28f16a3e4b2c5
Ben Horst <ben.l.horst@gmail.com>
2020-03-09 22:35:42 -0400

    posthumous copyright bump

--- src/gamebigmap.cpp
+++ src/gamebigmap.cpp
@@ -1,5 +1,5 @@
 //  Copyright (C) 2007 Ole Laursen
-//  Copyright (C) 2007-2011, 2014, 2015, 2017 Ben Asselstine
+//  Copyright (C) 2007-2011, 2014, 2015, 2017, 2020 Ben Asselstine
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
