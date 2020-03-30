02e1ef393f58b59c6d7998d2289218ce741dcf4c
Ben Horst <ben.l.horst@gmail.com>
2020-03-30 03:22:09 -0400

    making the temple editor do instant apply

--- src/editor/temple-editor-dialog.cpp
+++ src/editor/temple-editor-dialog.cpp
@@ -1,5 +1,5 @@
 //  Copyright (C) 2007 Ole Laursen
-//  Copyright (C) 2007, 2008, 2009, 2014 Ben Asselstine
+//  Copyright (C) 2007, 2008, 2009, 2014, 2020 Ben Asselstine
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -29,6 +29,8 @@
 #include "temple.h"
 #include "RenamableLocation.h"
 
+#define method(x) sigc::mem_fun(*this, &TempleEditorDialog::x)
+
 TempleEditorDialog::TempleEditorDialog(Gtk::Window &parent, Temple *t, CreateScenarioRandomize *randomizer)
  : LwEditorDialog(parent, "temple-editor-dialog.ui")
 {
@@ -36,38 +38,44 @@ TempleEditorDialog::TempleEditorDialog(Gtk::Window &parent, Temple *t, CreateSce
     temple = t;
     
     xml->get_widget("name_entry", name_entry);
+    name_entry->signal_changed().connect (method(on_name_changed));
     name_entry->set_text(temple->getName());
     name_entry->set_max_length (MAX_LENGTH_FOR_RUIN_NAME);
 
     xml->get_widget("description_entry", description_entry);
+    description_entry->signal_changed().connect
+      (method(on_description_changed));
     description_entry->set_text(temple->getDescription());
 
-    xml->get_widget("type_entry", type_entry);
-    type_entry->set_value(temple->getType());
+    xml->get_widget("type_spinbutton", type_spinbutton);
+    type_spinbutton->set_value(temple->getType());
+    type_spinbutton->signal_changed().connect (method(on_type_changed));
+    type_spinbutton->signal_insert_text().connect
+      (sigc::hide(sigc::hide(method(on_type_text_changed))));
+
     xml->get_widget("randomize_name_button", randomize_name_button);
     randomize_name_button->signal_clicked().connect(
 	sigc::mem_fun(this, &TempleEditorDialog::on_randomize_name_clicked));
 }
 
+void TempleEditorDialog::on_type_changed ()
+{
+  if (type_spinbutton->get_value() >= TEMPLE_TYPES)
+    type_spinbutton->set_value(TEMPLE_TYPES - 1);
+  else
+    temple->setType(int(type_spinbutton->get_value()));
+}
+
+void TempleEditorDialog::on_type_text_changed ()
+{
+  type_spinbutton->set_value(atoi(type_spinbutton->get_text().c_str()));
+  on_type_changed();
+}
+
 int TempleEditorDialog::run()
 {
-    dialog->show_all();
-    int response = dialog->run();
-
-    if (response == Gtk::RESPONSE_ACCEPT)	// accepted
-    {
-      Location *l = temple;
-      RenamableLocation *renamable_temple = static_cast<RenamableLocation*>(l);
-      renamable_temple->setName(name_entry->get_text());
-      temple->setType(type_entry->get_value_as_int());
-      renamable_temple->setDescription(description_entry->get_text());
-    }
-    else
-      {
-	if (name_entry->get_text() != Temple::getDefaultName())
-	  d_randomizer->pushRandomTempleName(name_entry->get_text());
-      }
-    return response;
+  dialog->show_all();
+  return dialog->run();
 }
 
 void TempleEditorDialog::on_randomize_name_clicked()
@@ -80,4 +88,16 @@ void TempleEditorDialog::on_randomize_name_clicked()
       name_entry->set_text(d_randomizer->popRandomTempleName());
       d_randomizer->pushRandomTempleName(existing_name);
     }
+}
+
+void TempleEditorDialog::on_description_changed ()
+{
+  temple->setDescription(description_entry->get_text());
+}
+
+void TempleEditorDialog::on_name_changed ()
+{
+  Location *l = temple;
+  RenamableLocation *renamable_temple = static_cast<RenamableLocation*>(l);
+  renamable_temple->setName(name_entry->get_text());
 }
--- src/editor/temple-editor-dialog.h
+++ src/editor/temple-editor-dialog.h
@@ -1,5 +1,5 @@
 //  Copyright (C) 2007 Ole Laursen
-//  Copyright (C) 2007, 2008, 2009, 2014 Ben Asselstine
+//  Copyright (C) 2007, 2008, 2009, 2014, 2020 Ben Asselstine
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -39,12 +39,16 @@ class TempleEditorDialog: public LwEditorDialog
  private:
     Gtk::Entry *name_entry;
     Gtk::Entry *description_entry;
-    Gtk::SpinButton *type_entry;
+    Gtk::SpinButton *type_spinbutton;
     Temple *temple;
     Gtk::Button *randomize_name_button;
     CreateScenarioRandomize *d_randomizer;
 
     void on_randomize_name_clicked();
+    void on_description_changed ();
+    void on_name_changed ();
+    void on_type_changed ();
+    void on_type_text_changed ();
 };
 
 #endif
