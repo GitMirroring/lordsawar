d9f3138768a886aee173745b5ab720a51b61fbec
Ben Horst <ben.l.horst@gmail.com>
2020-04-09 09:58:09 -0400

    explicitly running libtoolize because some distros don't do it automatically in autoreconf

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,5 @@
 #This script sets up the codebase so that it can be configured.
 autoreconf -fis
 intltoolize --automake
+libtoolize
 echo "Now type './configure' to prepare LordsAWar! for compilation."
