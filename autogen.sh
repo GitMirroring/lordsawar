8deb660dfd68c1b30ca39112b9ef215532f9d084
Ben Horst <ben.l.horst@gmail.com>
2020-04-09 09:11:28 -0400

    adding an explicit intltoolize because not all distros do it in the autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #This script sets up the codebase so that it can be configured.
 autoreconf -fis
+intltoolize --automake
 echo "Now type './configure' to prepare LordsAWar! for compilation."
