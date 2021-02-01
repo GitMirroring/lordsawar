334b077e32102fc9cdc9930de2f92bb0a924edea
Ben Asselstine <bing@nym.hush.com>
2021-02-01 09:55:14 -0500

    silencing warning

--- src/editor/image-editor-dialog.cpp
+++ src/editor/image-editor-dialog.cpp
@@ -315,7 +315,7 @@ bool ImageEditorDialog::installFile (TarFile *t, TarFileImage *im, Glib::ustring
     success =
       t->replaceFileInCfgFile (d_orig_target_filename, filename, newname);
   im->setName(newname);
-  bool b = im->load (t, newname);
+  im->load (t, newname);
   im->instantiateImages();
   return success;
 }
